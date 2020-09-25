85f8f8604daa6385e660662ec9add23f63f9ee4d
Haysam Bin Tahir <haysam.bin.tahir@gmail.com>
2020-09-26 00:05:21 +0500

    link text changed in app.js

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Link Changed to reflect change on Github.
         </a>
       </header>
     </div>
