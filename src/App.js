779d969eb94714dc5efee67ae2d38879393d756c
Haysam Bin Tahir <haysam.bin.tahir@gmail.com>
2020-09-26 04:10:06 +0500

    committing

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          This code was merged from another branch named "branch_for_blog"
+          Committing after blog component was pushed by another contributor. This version is a previous one.
         </p>
         <a
           className="App-link"
