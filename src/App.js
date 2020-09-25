636d73ab46c3c39750e43d9c720f2301e000002e
Haysam Bin Tahir <haysam.bin.tahir@gmail.com>
2020-09-26 03:50:44 +0500

    changed after cloning

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          This code was merged from another branch named "branch_for_blog"
+          Changed by another contributor
         </p>
         <a
           className="App-link"
