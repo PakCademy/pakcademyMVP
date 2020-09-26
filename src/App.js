5f8a26868a87ee362152de7d938cb3104d5b7d2a
Haysam Bin Tahir <haysam.bin.tahir@gmail.com>
2020-09-26 20:18:39 +0500

    Blog Header Added [Static Data being served - No API calls yet]

--- src/App.js
+++ src/App.js
@@ -1,26 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import BlogManager from "./components/Blog/BlogManager";
+
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-
-          Muzamil changed this text...
-
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Link Changed to reflect change on Github.
-        </a>
-      </header>
+      <BlogManager />
     </div>
   );
 }
