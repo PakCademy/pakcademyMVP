c33b8d66b5a976fbd0cc28853f3e6f57e9036b4e
Muzamil <muzamilsethar@gmail.com>
2020-09-27 03:12:23 +0500

    Blog Cards added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import BlogCard from './components/BlogCard/BlogCardUI/BlogCardUI';
+import BlogCardManager from './components/BlogCard/BlogCardManager/BlogCardManager';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Muzamil changed this text...
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Link Changed to reflect change on Github.
-        </a>
-      </header>
+      <BlogCardManager />
     </div>
   );
 }
